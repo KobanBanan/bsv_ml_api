38df65a634ded40d1ceef28e56eff67aa7c293b3
Fronzilla <abuseofinnocent@gmail.com>
2022-05-29 03:29:58 +0700

    work in progress

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from typing import List, Optional
-
 import uvicorn
 from fastapi import FastAPI
-#
-# from db.config import engine, Base, async_session
-# from db.dals.book_dal import BookDAL
-# from db.models.book import Book
+
+from db.config import engine, Base
+from routers import book_router
 
 app = FastAPI()
+app.include_router(book_router.router)
 
 
 @app.on_event("startup")
@@ -16,29 +14,6 @@ async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-
-
-@app.post("/books")
-async def create_book(name: str, author: str, release_year: int):
-    async with async_session() as session:
-        async with session.begin():
-            book_dal = BookDAL(session)
-            return await book_dal.create_book(name, author, release_year)
-
-
-@app.get("/books")
-async def get_all_books() -> List[Book]:
-    async with async_session() as session:
-        async with session.begin():
-            book_dal = BookDAL(session)
-            return await book_dal.get_all_books()
-
-@app.put("/books/{book_id}")
-async def update_book(book_id: int, name: Optional[str] = None, author: Optional[str] = None, release_year: Optional[int] = None):
-    async with async_session() as session:
-        async with session.begin():
-            book_dal = BookDAL(session)
-            return await book_dal.update_book(book_id, name, author, release_year)
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,20 +1,53 @@
 import asyncio
-import aiomysql
-
-
-async def test_example(loop):
-    pool = await aiomysql.create_pool(host='10.168.4.148',
-                                      user='fronzilla', password='GP8_4z8%8r++',
-                                      db='ML', loop=loop)
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("SELECT 42;")
-            print(cur.description)
-            (r,) = await cur.fetchone()
-            assert r == 42
-    pool.close()
-    await pool.wait_closed()
-
+import aioodbc
+import pandas as pd
+from catboost import CatBoostClassifier
+import pandas
+from utils import predict
 
 loop = asyncio.get_event_loop()
+
+from functools import partial
+
+dsn = 'Driver=SQL Server Native Client 11.0;Server=10.168.4.148;Database=ML;UID=fronzilla;PWD=GP8_4z8%8r++'
+
+cols = ['ClaimID',
+         'PersonAge',
+         'PersonSex',
+         'PersonRegistrationRegionNumber',
+         'BriefCaseName',
+         'LoanClassName',
+         'ContractTerm',
+         'ContractAmount',
+         'OtlNalPointID',
+         'ContractRegion',
+         'ContractCity',
+         'InitialCreditorType',
+         'TotalDebtAmount',
+         'BKILoanCount',
+         'BKILoanOverdueCount',
+         'BKIOrganizationCount',
+         'BKIDebtAmount']
+
+
+model = CatBoostClassifier().load_model('models/contact.cbm')
+
+# Sometimes you may want to reuse same connection parameters multiple times.
+# This can be accomplished in a way below using partial function
+connect = partial(aioodbc.connect, dsn=dsn, echo=True, autocommit=True)
+
+
+async def test_example(loop=None):
+    async with connect(loop=loop) as conn:
+        async with conn.cursor() as cur:
+            id_list = tuple([35146, 35208, 35217, 35229, 35286, 35307, 35321, 35357, 35364])
+            with open('sql/ContactenPersonNotebook.sql') as f:
+                sql = f.read().format(id_list)
+
+            await cur.execute(sql)
+            val = await cur.fetchall()
+
+            return predict(pd.DataFrame.from_records(val, columns=cols), model).set_index('ClaimID').T.to_dict()
+
+
 loop.run_until_complete(test_example(loop))
