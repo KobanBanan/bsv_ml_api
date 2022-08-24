168417c1e9ce2c8ebf93625e9f7dbf08c9bc9fde
Fronzilla <abuseofinnocent@gmail.com>
2022-08-24 21:29:18 +0700

    upload file

--- endpoints.py
+++ endpoints.py
@@ -1,5 +1,5 @@
 import asyncio
-from functools import partial
+from functools import partial, wraps
 from typing import List
 
 import aioodbc
@@ -13,6 +13,21 @@ dsn = 'Driver=SQL Server Native Client 11.0;Server=10.168.4.148;Database=ML;UID=
 connect = partial(aioodbc.connect, dsn=dsn, echo=True, autocommit=True)
 
 
+def clear_buffer_table():
+    def wrapper(func):
+        @wraps(func)
+        async def wrapped(*args):
+            async with connect(loop=asyncio.get_event_loop()) as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute('DELETE FROM ML.buffer.ClaimListAPI')
+                    return await func(*args)
+
+        return wrapped
+
+    return wrapper
+
+
+@clear_buffer_table()
 async def _get_30_seconds_predictions(
         ids: List[int], model: CatBoostClassifier = CatBoostClassifier().load_model('models/m30s.cbm')
 ):
@@ -24,8 +39,10 @@ async def _get_30_seconds_predictions(
     async with connect(loop=asyncio.get_event_loop()) as conn:
         async with conn.cursor() as cur:
             with open('sql/m30s.sql') as f:
-                sql = f.read().format(tuple(ids))
+                sql = f.read()
 
+            ids = [[x] for x in ids]
+            await cur.executemany('INSERT INTO ML.buffer.ClaimListAPI (ClaimID) VALUES (?);', ids)
             await cur.execute(sql)
             val = await cur.fetchall()
 
@@ -34,6 +51,7 @@ async def _get_30_seconds_predictions(
             ).set_index(PERSON_ID).T.to_dict()
 
 
+@clear_buffer_table()
 async def _get_give_promise_predictions(
         ids: List[int], model: CatBoostClassifier = CatBoostClassifier().load_model('models/mgp.cbm')
 ):
@@ -45,8 +63,10 @@ async def _get_give_promise_predictions(
     async with connect(loop=asyncio.get_event_loop()) as conn:
         async with conn.cursor() as cur:
             with open('sql/mgp.sql') as f:
-                sql = f.read().format(tuple(ids))
+                sql = f.read()
 
+            ids = [[x] for x in ids]
+            await cur.executemany('INSERT INTO ML.buffer.ClaimListAPI (ClaimID) VALUES (?);', ids)
             await cur.execute(sql)
             val = await cur.fetchall()
 
@@ -55,6 +75,7 @@ async def _get_give_promise_predictions(
             ).set_index(PERSON_ID).T.to_dict()
 
 
+@clear_buffer_table()
 async def _get_keep_promise_predictions(
         ids: List[int], model: CatBoostClassifier = CatBoostClassifier().load_model('models/mkp.cbm')
 ):
@@ -67,8 +88,10 @@ async def _get_keep_promise_predictions(
     async with connect(loop=asyncio.get_event_loop()) as conn:
         async with conn.cursor() as cur:
             with open('sql/mkp.sql') as f:
-                sql = f.read().format(tuple(ids))
+                sql = f.read()
 
+            ids = [[x] for x in ids]
+            await cur.executemany('INSERT INTO ML.buffer.ClaimListAPI (ClaimID) VALUES (?);', ids)
             await cur.execute(sql)
             val = await cur.fetchall()
 
--- sql/m30s.sql
+++ sql/m30s.sql
@@ -1,95 +1,93 @@
 WITH RawBriefCaseTable AS
-         (SELECT DISTINCT PersonID,
-                          CreditID,
-                          ThirdID,
-                          StrahID,
-                          ContractDate,
-                          ContractAmount,
-                          MainDebtAmount,
-                          PercentAmount,
-                          FineAmount,
-                          ISNULL(MainDebtAmount, 0) + ISNULL(PercentAmount, 0) +
-                          ISNULL(FineAmount, 0)                  [TotalDebtAmount],
-                          CAST(BKILoanCount AS DECIMAL(5, 1)) AS [BKILoanCount],
-                          BKILoanOverdueCount,
-                          BKIOrganizationCount,
-                          BKIDebtAmount,
-                          BKILastPaymentDate
+	     (SELECT DISTINCT PersonID,
+	                      CreditID,
+	                      ThirdID,
+	                      StrahID,
+	                      ContractDate,
+	                      ContractAmount,
+	                      MainDebtAmount,
+	                      PercentAmount,
+	                      FineAmount,
+	                      ISNULL(MainDebtAmount, 0) + ISNULL(PercentAmount, 0) +
+	                      ISNULL(FineAmount, 0)                  [TotalDebtAmount],
+	                      CAST(BKILoanCount AS DECIMAL(5, 1)) AS [BKILoanCount],
+	                      BKILoanOverdueCount,
+	                      BKIOrganizationCount,
+	                      BKIDebtAmount,
+	                      BKILastPaymentDate
 
-          FROM reference.BriefCase bc
+	      FROM reference.BriefCase bc
 
-          WHERE bc.ClaimID IN {}
+		           INNER JOIN buffer.ClaimListAPI cl ON bc.ClaimID = cl.ClaimID
 
-          AND ContractAmount > 1
-            AND (ISNULL(MainDebtAmount, 0) + ISNULL(PercentAmount, 0) +
-                 ISNULL(FineAmount, 0)) > 1),
+	      WHERE ContractAmount > 1
+		    AND (ISNULL(MainDebtAmount, 0) + ISNULL(PercentAmount, 0) +
+		         ISNULL(FineAmount, 0)) > 1),
 
      PersonContractInfo AS
-         (SELECT PersonID,
-                 COUNT(CreditID) [CreditCount],
-                 COUNT(ThirdID)  [ThirdCount],
-                 COUNT(StrahID)  [InsuranceCount]
+	     (SELECT PersonID,
+	             COUNT(CreditID) [CreditCount],
+	             COUNT(ThirdID)  [ThirdCount],
+	             COUNT(StrahID)  [InsuranceCount]
 
-          FROM RawBriefCaseTable bc
+	      FROM RawBriefCaseTable bc
 
-          GROUP BY PersonID),
+	      GROUP BY PersonID),
 
      PersonContractType AS
-         (SELECT PersonID,
+	     (SELECT PersonID,
 
-                 CASE
-                     WHEN CreditCount > 0 AND ThirdCount > 0 AND InsuranceCount > 0 AND ThirdCount != InsuranceCount
-                         THEN 7 --'Mixed'
+	             CASE
+		             WHEN CreditCount > 0 AND ThirdCount > 0 AND InsuranceCount > 0 AND ThirdCount != InsuranceCount
+			             THEN 7 --'Mixed'
 
-                     WHEN CreditCount > 0 AND InsuranceCount = 0 AND ThirdCount = 0 THEN 1 --'ON Only'
-                     WHEN CreditCount = 0 AND ThirdCount > 0 AND InsuranceCount = 0 THEN 2 --'Third Only'
-                     WHEN CreditCount = 0 AND InsuranceCount > 0 AND ThirdCount = InsuranceCount
-                         THEN 3 --'Insurance Only'
+		             WHEN CreditCount > 0 AND InsuranceCount = 0 AND ThirdCount = 0 THEN 1 --'ON Only'
+		             WHEN CreditCount = 0 AND ThirdCount > 0 AND InsuranceCount = 0 THEN 2 --'Third Only'
+		             WHEN CreditCount = 0 AND InsuranceCount > 0 AND ThirdCount = InsuranceCount
+			             THEN 3 --'Insurance Only'
 
-                     WHEN CreditCount > 0 AND ThirdCount > 0 AND InsuranceCount = 0 THEN 4 --'ON And Third'
-                     WHEN CreditCount > 0 AND InsuranceCount > 0 AND ThirdCount = InsuranceCount
-                         THEN 5 --'ON And Insurance'
+		             WHEN CreditCount > 0 AND ThirdCount > 0 AND InsuranceCount = 0 THEN 4 --'ON And Third'
+		             WHEN CreditCount > 0 AND InsuranceCount > 0 AND ThirdCount = InsuranceCount
+			             THEN 5 --'ON And Insurance'
 
-                     WHEN CreditCount = 0 AND ThirdCount > 0 AND InsuranceCount > 0 AND ThirdCount != InsuranceCount
-                         THEN 6 --'Third And Insurance'
+		             WHEN CreditCount = 0 AND ThirdCount > 0 AND InsuranceCount > 0 AND ThirdCount != InsuranceCount
+			             THEN 6 --'Third And Insurance'
 
-                     ELSE
-                         999 --'HZ'
-                     END [PersonContractType]
+		             ELSE
+			             999 --'HZ'
+	             END [PersonContractType]
 
-          FROM PersonContractInfo),
+	      FROM PersonContractInfo),
 
      DataSet AS
-         (SELECT DISTINCT bc.PersonID,
-                          DATEDIFF(YEAR, pers.PersonBirthDate, GETDATE())                            [PersonAge],
-                          CAST(pers.PersonGenderMale AS TinyInt)                                  AS PersonGenderMale,
-                          pers.PersonRegionRegistrationNumber,
-                          pct.PersonContractType,
+	     (SELECT DISTINCT bc.PersonID,
+	                      DATEDIFF(YEAR, pers.PersonBirthDate, GETDATE())                            [PersonAge],
+	                      CAST(pers.PersonGenderMale AS TinyInt)                                  AS PersonGenderMale,
+	                      pers.PersonRegionRegistrationNumber,
+	                      pct.PersonContractType,
 
-                          DATEDIFF(MONTH, MIN(ContractDate), MAX(ContractDate))                      [MonthBetweenFirstLastContract],
+	                      DATEDIFF(MONTH, MIN(ContractDate), MAX(ContractDate))                      [MonthBetweenFirstLastContract],
 
-                          COUNT(*)                                                                   [OurPersonContractCount],
+	                      COUNT(*)                                                                   [OurPersonContractCount],
 
-                          MAX(ContractAmount) / MIN(ContractAmount)                                  [OurMinMaxContractAmountRatio],
+	                      MAX(ContractAmount) / MIN(ContractAmount)                                  [OurMinMaxContractAmountRatio],
 
-                          MAX(TotalDebtAmount) / MIN(TotalDebtAmount)                                [OurMinMaxTotalDebtAmountRatio],
+	                      MAX(TotalDebtAmount) / MIN(TotalDebtAmount)                                [OurMinMaxTotalDebtAmountRatio],
 
-                          CAST(IIF(SUM(BKILoanCount) = 0, 0, SUM(BKILoanOverdueCount) /
-                                                             SUM(BKILoanCount)) AS Decimal(7, 5)) AS [BKILoanOverdueCountRatio],
+	                      CAST(IIF(SUM(BKILoanCount) = 0, 0, SUM(BKILoanOverdueCount) /
+	                                                         SUM(BKILoanCount)) AS Decimal(7, 5)) AS [BKILoanOverdueCountRatio],
 
-                          SUM(BKIDebtAmount)                                                      AS [BKIDebtAmount],
+	                      SUM(BKIDebtAmount)                                                      AS [BKIDebtAmount],
 
-                          IIF(MAX(BKILastPaymentDate) IS NULL, 0, 1)                                 [BKILastPaymentInfo]
+	                      IIF(MAX(BKILastPaymentDate) IS NULL, 0, 1)                                 [BKILastPaymentInfo]
 
-          FROM RawBriefCaseTable bc
+	      FROM RawBriefCaseTable bc
 
-                   LEFT JOIN otlnal.Person pers ON bc.PersonID = pers.PersonID
-                   LEFT JOIN PersonContractType pct ON bc.PersonID = pct.PersonID
+		           LEFT JOIN otlnal.Person pers ON bc.PersonID = pers.PersonID
+		           LEFT JOIN PersonContractType pct ON bc.PersonID = pct.PersonID
 
-          GROUP BY bc.PersonID, pers.PersonFIO, pers.PersonBirthDate, pers.PersonGenderMale,
-                   pers.PersonRegionRegistrationNumber,
-                   pct.PersonContractType)
+	      GROUP BY bc.PersonID, pers.PersonFIO, pers.PersonBirthDate, pers.PersonGenderMale,
+	               pers.PersonRegionRegistrationNumber,
+	               pct.PersonContractType)
 
-SELECT DISTINCT ds.*
-FROM DataSet ds
-ORDER BY PersonID
+SELECT DISTINCT ds.* FROM DataSet ds ORDER BY PersonID
