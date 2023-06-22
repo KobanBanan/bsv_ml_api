b73d354d6ac5ed7becb2f8c5d3e18a29657d6da5
alexeynikitin <abuseofinnocent@gmail.com>
2023-06-22 11:24:57 +0200

    csbi

--- endpoints.py
+++ endpoints.py
@@ -267,7 +267,8 @@ async def _send_fis_request(batch_uuid: str) -> Dict:
     # response = requests.post(
     response = requests.post('http://10.115.0.40:8080/platform/rs2/rest/endpoint/exec_document_motion',
                              headers={'Content-Type': 'application/json; charset=UTF-8'},
-                             json=json.dumps(res, ensure_ascii=False, default=str).encode('utf8').decode('utf8')
+                             json=json.dumps(res, ensure_ascii=False, default=str).encode('utf8').decode('utf8'),
+                             timeout=120
 
                              )
 
