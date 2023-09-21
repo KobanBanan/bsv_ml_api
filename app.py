49055fe528f8546ce66a1bb360e2ff71008384ca
alexeynikitin <abuseofinnocent@gmail.com>
2023-09-21 10:10:17 +0200

    timeout_keep_alive

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(routers.router)
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", port=1111, host='127.0.0.1')
+    uvicorn.run("app:app", port=1111, host='127.0.0.1', timeout_keep_alive=180)
